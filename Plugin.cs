9ea7ba508aa82a422a7e64a642b3b21142e7b6f8
scheals <95084627+scheals@users.noreply.github.com>
2024-12-18 23:43:28 +0100

    feat: Stick Gatherer's cornerstone

--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,12 @@
-﻿using BepInEx;
+﻿using ATS_API.Effects;
+using ATS_API.Helpers;
+using BepInEx;
 using Eremite;
 using Eremite.Controller;
+using Eremite.Controller.Generator;
+using Eremite.Model;
+using Eremite.Model.Effects;
+using Eremite.Model.Effects.Hooked;
 using HarmonyLib;
 
 namespace ModTemplate
@@ -27,7 +33,48 @@ namespace ModTemplate
             // Your main entry point to access this data will be `Serviceable.Settings` or `MainController.Instance.Settings`
             Instance.Logger.LogInfo($"Performing game initialization on behalf of {PluginInfo.PLUGIN_GUID}.");
             Instance.Logger.LogInfo($"The game has loaded {MainController.Instance.Settings.effects.Length} effects.");
+            //foreach (EffectModel effectModel in MB.Settings.effects)
+            //{
+            //    if (effectModel.IsPerk)
+            //    {
+            //        Instance.Logger.LogInfo(effectModel.GetType().Name);
+            //        Instance.Logger.LogInfo("Found perk with name: " + effectModel.name + " and display name " + effectModel.displayName);
+            //    }
+
+
+
+            //}
+            foreach (LocaText cityName in MB.Settings.worldConfig.citiesNames)
+            {
+                Instance.Logger.LogInfo("City object: " + cityName + " City name: " + cityName.Text + " City key: " + cityName.key);
+            }
+
         }
+
+        [HarmonyPatch(typeof(MainController), nameof(MainController.InitReferences))]
+        [HarmonyPostfix]
+        private static void PostSetupMainController()
+        {
+
+            string effectName = "Stick Gatherers";
+            string effectIconPath = "StickStick.jpg";
+            int amount = 7;
+
+            GoodModel woodGoodModel = MB.Settings.GetGood(GoodsTypes.Mat_Raw_Wood.ToName());
+            GoodRef woodGoodRef = new() { good = woodGoodModel, amount = amount };
+            EffectBuilder<GoodsPerMinEffectModel> builder = new(PluginInfo.PLUGIN_GUID, effectName, effectIconPath);
+            builder.SetRarity(EffectRarity.Epic);
+            builder.SetPositive(true);
+            builder.SetDrawLimit(5);
+            builder.SetAvailableInAllBiomesAndSeasons();
+            builder.SetObtainedAsCornerstone();
+            builder.SetLabel("Scheals' Perk");
+            builder.SetDisplayName(effectName);
+            builder.SetDescription($"Gain {amount} of Wood {woodGoodModel.GetTextIcon()} every minute.");
+            builder.EffectModel.good = woodGoodRef;
+
+        }
+
 
         [HarmonyPatch(typeof(GameController), nameof(GameController.StartGame))]
         [HarmonyPostfix]
@@ -38,12 +85,31 @@ namespace ModTemplate
             var isNewGame = MB.GameSaveService.IsNewGame();
             Instance.Logger.LogInfo($"Entered a game. Is this a new game: {isNewGame}.");
 
+
+
             if (isNewGame)
             {
                 SO.EffectsService.GrantWildcardPick(1);
                 Plugin.Instance.Logger.LogInfo("New wildcard pick granted for new game");
 
+                EffectModel woodCutter = MB.Settings.GetEffect("Resolve for Glade");
+                woodCutter.Apply();
+                Plugin.Instance.Logger.LogInfo("Resolve for Glade added.");
+
+                EffectModel stickStick = MB.Settings.GetEffect($"{PluginInfo.PLUGIN_GUID}_Stick Gatherers");
+                stickStick.Apply();
+                Plugin.Instance.Logger.LogInfo("Stick Gatherers added.");
+
             }
         }
+        [HarmonyPatch(typeof(GameLoader), nameof(GameLoader.LoadGame))]
+        [HarmonyPostfix]
+        private static void SetUpInitialState()
+        {
+            MB.MetaStateService.Perks.bonusCornerstonesRerolls = 99;
+            Plugin.Instance.Logger.LogInfo("Cornerstone rerolls set to 99.");
+        }
+
+
     }
 }
